c16fac2e2b436e300a2875679f8e19f535e2078c
lijingxin <903306336@qq.com>
2018-08-12 18:52:19 +0800

    从excel统计每个imei都有的app

---
+++ src/main/python/StatisticsApp.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+
+import sys
+import xlrd
+
+file = r"/Users/chenming/ljx_workspace/python/src/main/resource/imei.xlsx"  # 打开指定路径中的文件
+
+imei = xlrd.open_workbook(file)  # 实例化
+sheet0 = imei.sheet_by_index(0)  # 获得第一页
+
+col0 = sheet0.col_values(0)  # 获取第一列的值
+col1 = sheet0.col_values(1)
+col2 = sheet0.col_values(2)
+col3 = sheet0.col_values(3)
+app = []
+
+for i in range(1, len(col0)):
+    if col0[i] in col1 and col0[i] in col2 and col0[i] in col3:
+        app.append(col0[i])
+
+print(app)
